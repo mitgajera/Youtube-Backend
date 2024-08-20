eaa47f524b1a11d42465de4ece8247ebc8897904
mitgajera09 <gajeramit180@gmail.com>
2024-08-20 18:44:18 +0530

    "Refactor app.js, user.controller.js, index.js, user.routes.js, and asyncHandler.js files"

--- source/index.js
+++ source/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 })
-
-const app = express();
 
 const DB_NAME = "../constants.js"
 const port = process.env.PORT || 8000;
--- source/routes/user.routes.js
+++ source/routes/user.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { registerUser } from "../controllers/user.controller.js";
+import { Router } from 'express';
 
 
-const router = express.Router();
-router.post('/register', asyncHandler(registerUser));
+const router = Router()
+
+router.route("/register").post(registerUser)
+
 export default router;
